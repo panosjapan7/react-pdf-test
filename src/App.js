e8bc933b2a45472a51729fc07eaa24e08e460d79
panosjapan7 <panosjapan@gmail.com>
2022-07-21 09:41:16 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,29 +2,11 @@ import { PDFDownloadLink, ReactPDF, PDFViewer } from "@react-pdf/renderer";
 import BasicDocument from "./components/BasicDocument";
 
 function App() {
-  // <PDFViewer>
-  //   <PDFFile />
-  // </PDFViewer>
   return (
     <div >
         <BasicDocument /> 
-      {/* // <PDFDownloadLink document={<PDFFile />} fileName="FORM">
-        
-      //   <button>Download</button>
-      // </PDFDownloadLink>
-      // <PDFFile /> */}
-
     </div>
   );
 }
 
 export default App;
-
-
-/* {({loading}) => 
-          loading ? (
-            <button>Loading Document...</button> 
-            )  : (
-              <button>Download</button>
-            ) 
-        } */
